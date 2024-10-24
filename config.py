2f9ee3e967b6a889c3044b8b235ef2712b96353a
MovieFirebots <kesavansivakumar11@gmail.com>
2024-10-24 08:01:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nThis is AnimeFireTamil's private files Sharing bot and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "1775977570,1386428990").split()):
+    for x in (os.environ.get("ADMINS", "1775977570 1386428990").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
